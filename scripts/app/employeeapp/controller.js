fa952699f4ff59b2d009bc5e94bf3e75d70c8235
cpsa3 <1090303279@qq.com>
2015-03-06 17:47:42 +0800

    integration karma-coverage

---
+++ scripts/app/employeeapp/controller.js
@@ -0,0 +1,11 @@
+﻿
+
+define(["angular", "services"], function (angular) {
+    return angular.module("EmployeeApp.controllers",[])
+        .controller("EmployeeAppController", [
+            "$scope", "$rootScope",
+            function ($scope, $rootScope) {
+                $scope.test="321";
+            }
+        ]);
+});
