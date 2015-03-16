d55a24042765f4982d9b49a53fd21d6da095887e
cpsa3 <1090303279@qq.com>
2015-03-17 00:34:57 +0800

    add angular msgBus service

--- scripts/app/common/services.js
+++ scripts/app/common/services.js
@@ -6,5 +6,37 @@ define(["angular"], function(angular) {
             return function() {
                 return "hello world";
             };
-        });
+        })
+        .service('messageService', ['$rootScope', function($rootScope) {
+            var sharedService = {};
+
+            sharedService.publish = function(msg, data) {
+                data = data || {};
+                $rootScope.$broadcast(msg, data);
+            };
+
+            sharedService.subscribe = function(msg, scope, func) {
+                scope.$on(msg, func);
+            };
+
+            return sharedService;
+        }])
+        .service('messageBus', ['$rootScope', function($rootScope) {
+            var messageBus = {};
+
+            messageBus.publish = function(msg, data) {
+                data = data || {};
+                $rootScope.$emit(msg, data);
+            };
+
+            messageBus.subscribe = function(msg, scope, func) {
+                var unbind = $rootScope.$on(msg, func);
+                if (scope) {
+                    scope.$on('$destroy', unbind);
+                }
+                return unbind;
+            };
+
+            return messageBus;
+        }]);
 });
