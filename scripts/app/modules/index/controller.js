ca44b142d49cd34f2e54422caf23e2a23633ab74
cpsa3 <1090303279@qq.com>
2015-03-17 17:06:27 +0800

    unsubscribe event

--- scripts/app/modules/index/controller.js
+++ scripts/app/modules/index/controller.js
@@ -3,19 +3,24 @@
 define(["angular"], function(angular) {
     return angular.module("IndexApp.controller", ['Common.services'])
         .controller("IndexAppController", [
-            "$scope", "$rootScope", 'messageBus',
-            function($scope, $rootScope, messageBus) {
+            "$scope", "$rootScope", 'messageBus', '$filter',
+            function($scope, $rootScope, messageBus, $filter) {
                 $scope.desp = "this is index page.";
+
+
+                var currentTime = $filter('date')(new Date(), 'yyyy-MM-dd HH:mm:ss');
 
                 var data = [{
                     url: "student",
-                    view: "学员管理1"
+                    view: "学员 - " + currentTime
                 }, {
                     url: "demo",
-                    view: "测试1"
+                    view: "测试 - " + currentTime
+                }, {
+                    url: "index",
+                    view: "首页 - " + currentTime
                 }];
                 messageBus.publish('index.load', data);
-                data[0].view = 'xxx';
             }
         ]);
 });
--- scripts/app/modules/rootapp/controller.js
+++ scripts/app/modules/rootapp/controller.js
@@ -1,9 +1,4 @@
 ﻿"use strict";
-/**
- * author :小潘
- * time: 2015年3月3日 17:45:17
- * description:  主入口控制器 放置核心业务处理函数
- */
 
 define(["angular"], function(angular) {
     return angular.module("CourseCommonApp.controllers", ['Common.services'])
@@ -13,19 +8,24 @@ define(["angular"], function(angular) {
 
                 $scope.navModel = [{
                     url: "student",
-                    view: "学员管理"
+                    view: "学员"
                 }, {
                     url: "demo",
                     view: "测试"
+                }, {
+                    url: "index",
+                    view: "首页"
                 }];
+                
                 $scope.navdemo = 123;
 
                 //订阅index.load事件
                 var unsubcribe = messageBus.subscribe('index.load', $scope, function(event, data) {
                     $scope.navModel = data;
+                    //首次触发后取消订阅
+                    unsubcribe();
                 });
 
-                //unsubcribe();
             }
         ]);
 });
