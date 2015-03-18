fdeb48d9e9d857fa1bedf825c5c1f24e3f1042f5
cpsa3 <1090303279@qq.com>
2015-03-19 00:55:44 +0800

    resolve the templateCache issue

--- scripts/test/spec/rootapp/appSpec.js
+++ scripts/test/spec/rootapp/appSpec.js
@@ -11,26 +11,18 @@ define(['app'], function(app) {
 
             module('ngTemplates');
             module('CourseCommonApp');
+            module("CourseCommonApp.routes");
 
 
-            //preloadTpl('modules/index/index.html')
-            //module('modules/index/index.html');
+            //preloadTpl('modules/demoapp/demo.html?undefined')
 
             inject(function(_$rootScope_, _$state_, _$injector_, $templateCache, _$httpBackend_) {
                 $rootScope = _$rootScope_;
                 $state = _$state_;
                 $injector = _$injector_;
                 $httpBackend = _$httpBackend_;
-                $templateCache.put('modules/demoapp/demo.html',
-                    '<div>\n' +
-                    '    测试测试\n' +
-                    '</div>\n' +
-                    '\n' +
-                    '{{test}}');
-                // We need add the template entry into the templateCache if we ever
-                // specify a templateUrl
-                //$templateCache.put('template.html', '');
-                //$templateCache.put('modules/index/index.html', '');
+                //require or karma by request the template by xxx.html?undefined fuck it!
+                $templateCache.put('modules/demoapp/demo.html?undefined','');
             })
         });
 
@@ -38,12 +30,10 @@ define(['app'], function(app) {
             expect($state.href('index')).toEqual('#/index');
         });
 
-        // it('should resolve data', function() {
-
-        //     $state.go('demo');
-        //     $rootScope.$digest();
-        //     expect($state.current.name).toBe('demo');
-
-        // });
+        it('should resolve data', function() {
+            $state.go('demo');
+            $rootScope.$digest();
+            expect($state.current.name).toBe('demo');
+        });
     });
 });
