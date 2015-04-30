2a01a4dbf2c405db5e8903f6b311d9f23517cba5
cpsa3 <1090303279@qq.com>
2015-04-30 14:44:12 +0800

    add singleHttpFactory service

--- scripts/app/common/services.js
+++ scripts/app/common/services.js
@@ -1,45 +1,45 @@
 ﻿"use strict";
 
-define(["angular"], function(angular) {
+define(["angular"], function (angular) {
     return angular.module("Common.services", [])
-        .service("helloWorld", function() {
-            return function() {
+        .service("helloWorld", function () {
+            return function () {
                 return "hello world";
             };
         })
-        .service("guidService", ["$q", function($q) {
+        .service("guidService", ["$q", function ($q) {
             return {
-                get: function() {
-                    var S4 = function() {
+                get: function () {
+                    var S4 = function () {
                         return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
                     };
                     return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
                 }
             };
         }])
-        .service('messageService', ['$rootScope', function($rootScope) {
+        .service('messageService', ['$rootScope', function ($rootScope) {
             var sharedService = {};
 
-            sharedService.publish = function(msg, data) {
+            sharedService.publish = function (msg, data) {
                 data = angular.copy(data) || {};
                 $rootScope.$broadcast(msg, data);
             };
 
-            sharedService.subscribe = function(msg, scope, func) {
+            sharedService.subscribe = function (msg, scope, func) {
                 scope.$on(msg, func);
             };
 
             return sharedService;
         }])
-        .service('messageBus', ['$rootScope', function($rootScope) {
+        .service('messageBus', ['$rootScope', function ($rootScope) {
             var messageBus = {};
 
-            messageBus.publish = function(msg, data) {
+            messageBus.publish = function (msg, data) {
                 data = angular.copy(data) || {};
                 $rootScope.$emit(msg, data);
             };
 
-            messageBus.subscribe = function(msg, scope, func) {
+            messageBus.subscribe = function (msg, scope, func) {
                 var unsubscribe = $rootScope.$on(msg, func);
                 if (scope) {
                     //remove the listener when $scope is destroyed
@@ -51,15 +51,52 @@ define(["angular"], function(angular) {
 
             return messageBus;
         }])
-        .factory('singlePromiseWapper', ['$q', function($q) {
+        .factory('httpRequestWapper', ['$http', '$q', function ($http, $q) {
+            return {
+                get: function (url) {
+                    var deferred = $q.defer();
+
+                    $http.get(url).success(function (result) {
+                        if (result.status === 1) {
+                            deferred.resolve(result.data);
+                        } else {
+                            deferred.reject(result.message);
+                        }
+                    }).error(function (reason) {
+                        deferred.reject(reason);
+                    });
+
+                    return deferred.promise;
+                },
+                post: function (url, arg) {
+                    var deferred = $q.defer();
+
+                    $http.post(url, arg).success(function (result) {
+                        if (result.status === 1) {
+                            deferred.resolve(result.data);
+                        } else {
+                            deferred.reject(result.message);
+                        }
+                    }).error(function (reason) {
+                        deferred.reject(reason);
+                    });
+
+                    return deferred.promise;
+                },
+            };
+        }])
+        .factory('singlePromiseWapper', ['$q', function ($q) {
             /**
              * [description]
-             * @param  {Function} fn [is a function whitch return a promise]
+             * @param  {Function}  [first arg is a function whitch return a promise]
              * @return {[function]} [is a function which return a promise]
              */
-            return function(fn) {
+            return function () {
                 var isProcessing = false;
-                return function() {
+                // default args
+                var args = Array.prototype.slice.call(arguments);
+                var func = args.shift();
+                return function () {
                     if (isProcessing) {
                         console.log('isProcessing...');
                         return;
@@ -67,10 +104,10 @@ define(["angular"], function(angular) {
 
                     isProcessing = true;
                     var deferred = $q.defer();
-                    fn.apply(null, arguments).then(function(result) {
+                    func.apply(null, args.concat(Array.prototype.slice.call(arguments))).then(function (result) {
                         isProcessing = false;
                         deferred.resolve(result);
-                    }, function(reason) {
+                    }, function (reason) {
                         isProcessing = false;
                         deferred.reject(reason);
                     });
@@ -79,56 +116,46 @@ define(["angular"], function(angular) {
                 };
             };
         }])
-        .factory('httpWapper', ['$q', 'singlePromiseWapper', function($q, singlePromiseWapper) {
-            //return: a function which return a promise
+        .factory('singleHttpFactory', ['httpRequestWapper', 'singlePromiseWapper', function (httpRequestWapper, singlePromiseWapper) {
             return {
                 /**
+                 * description:
+                 * get a function which is prohibited duplicate request.
+                 * 
                  * usage: 
-                 * var netService = httpWapper.singleGet(url);
-                 * netService();
+                 * var getNetService = singleHttpFactory.get(url);
+                 * getNetService();
+                 *
+                 * var postNetService = singleHttpFactory.post(url);
+                 * postNetService(args);
                  */
-                singleGet: function(url) {
-                    return singlePromiseWapper(function() {
-                        return $http.get(url);
-                    });
+                get: function (url) {
+                    return singlePromiseWapper(httpRequestWapper.get, url);
                 },
-                singlePost: function(url, args) {
-                    return singlePromiseWapper(function() {
-                        return $http.post(url, args);
-                    });
+                post: function (url) {
+                    return singlePromiseWapper(httpRequestWapper.post, url);
                 }
             };
         }])
-        .factory('demoNetService', ['$q', 'httpWapper', function($q, httpWapper) {
+        
+        .factory('demoNetService', ['$q', 'singleHttpFactory', function ($q, singleHttpFactory) {
             /**
-             * old usage:
+             * usage:
              * demoNetService.getCurrentUser().then();
+             *
              * demoNetService.saveCurrentUser(args).then();
-             *
-             * new usage:
-             * var getCurrentUserService = demoNetService.getCurrentUser();
-             * getCurrentUserService().then();
-             *
-             * var saveCurrentUserService = demoNetService.saveCurrentUser(args);
-             * saveCurrentUserService().then();
              */
             return {
-                getCurrentUser: function() {
-                    var url = '/UserService/GetCurrentUser';
-                    return httpWapper.singleGet(url);
-                },
-                saveCurrentUser: function(args) {
-                    var url = '/UserService/SaveCurrentUser';
-                    return httpWapper.singlePost(url, args);
-                }
+                getCurrentUser: singleHttpFactory.get('/UserService/GetCurrentUser'),
+                saveCurrentUser: singleHttpFactory.post('/UserService/SaveCurrentUser')
             };
         }])
-        .factory('format', ['$q', function($q) {
+        .factory('format', ['$q', function ($q) {
             //Method : format("And the {1} want to know whose {2} you {3}", "papers", "shirt", "wear");
-            return function(string) {
+            return function (string) {
                 var args = arguments;
                 var pattern = new RegExp("{([1-" + arguments.length + "])}", "g");
-                return String(string).replace(pattern, function(match, index) {
+                return String(string).replace(pattern, function (match, index) {
                     return args[index];
                 });
             };
